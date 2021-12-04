6ce00bcc1f16159bc135d6093c670a37213faf62
Benjamin Chinwe <chisoftltd@gmail.com>
2021-12-04 09:25:38 +0000

    Ascii art and looping

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from replit import clear
 from random import randint
-from art import logo
+from art import logo2
 
-EASY_LEVEL_TURNS = 10
-HARD_LEVEL_TURNS = 5
+clear()
+GAME_LEVEL_EASY = 10
+GAME_LEVEL_HARD = 5
 
 #Function to check user's guess against actual answer.
 def check_answer(guess, answer, turns):
@@ -18,37 +20,47 @@ def check_answer(guess, answer, turns):
 
 #Make function to set difficulty.
 def set_difficulty():
-  level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
   if level == "easy":
-    return EASY_LEVEL_TURNS
+    return GAME_LEVEL_EASY
   else:
-    return HARD_LEVEL_TURNS
-
+    return GAME_LEVEL_HARD
+	
 def game():
-  print(logo)
-  #Choosing a random number between 1 and 100.
-  print("Welcome to the Number Guessing Game!")
-  print("I'm thinking of a number between 1 and 100.")
-  answer = randint(1, 100)
-  print(f"Pssst, the correct answer is {answer}") 
+	print(logo2)
+	#Choosing a random number between 1 and 100.
+	print("Welcome to the Number Guessing Game!")
+	print("I'm thinking of a number between 1 and 100.")
+	answer = randint(1, 100)
+	#print(f"Pssst, the correct answer is {answer}") 
 
-  turns = set_difficulty()
-  #Repeat the guessing functionality if they get it wrong.
-  guess = 0
-  while guess != answer:
-    print(f"You have {turns} attempts remaining to guess the number.")
+	turns = set_difficulty()
+	#Repeat the guessing functionality if they get it wrong.
+	guess = 0
+	while guess != answer:
+		print(f"You have {turns} attempts remaining to guess the number.")
 
-    #Let the user guess a number.
-    guess = int(input("Make a guess: "))
+		#Let the user guess a number.
+		guess = int(input("Make a guess: "))
 
-    #Track the number of turns and reduce by 1 if they get it wrong.
-    turns = check_answer(guess, answer, turns)
-    if turns == 0:
-      print("You've run out of guesses, you lose.")
-      return
-    elif guess != answer:
-      print("Guess again.")
+		#Track the number of turns and reduce by 1 if they get it wrong.
+		turns = check_answer(guess, answer, turns)
+		if turns == 0:
+			print("You've run out of guesses, you lose.")
+			print(f"Pssst, the correct answer is {answer}")
+			return
+		elif guess != answer:
+			print("Guess again.")
 
-
-game()
-
+play_again = True
+while play_again:
+	game()
+	reply = input("Do you want play again: \'y\' or \'n\'? ").lower()
+	if reply == 'n':
+		play_again = False
+	elif reply != 'y':
+		reply = input("Try again: Choose \'y\' or \'n\'? ").lower()
+		if reply == 'n':
+			play_again = False
+	if reply == 'y':
+		clear()
